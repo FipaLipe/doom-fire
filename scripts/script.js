15c82fdb94dfcbe9251beb0be78479bace6c2944
Filipi Martins <filipi.martinss@hotmail.com>
2025-11-07 14:22:29 -0300

    feat: implement createFireData funcionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
 function start() {
   console.log("Script has started!");
+
+  let fireDataWidth = 10;
+  let fireDataHeight = 10;
+  let fireData = createFireData(fireDataWidth, fireDataHeight);
 }
 
-function createFireData() {}
+function createFireData(width, height) {
+  let totalCellCount = width * height;
+
+  let newFireData = [];
+  for (let i = 0; i < totalCellCount; i++) {
+    newFireData.push(0);
+  }
+
+  return newFireData;
+}
 
 function updateFire() {}
 
 function renderFire() {}
+
+start();
